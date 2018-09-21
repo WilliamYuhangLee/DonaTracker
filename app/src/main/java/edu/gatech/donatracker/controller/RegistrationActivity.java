2deeaec47eb7fbb0969307a41f60b406f8c7bca5
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-09-20 22:09:32 -0400

    create RegistrationActivity

---
+++ app/src/main/java/edu/gatech/donatracker/controller/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.donatracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.donatracker.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
