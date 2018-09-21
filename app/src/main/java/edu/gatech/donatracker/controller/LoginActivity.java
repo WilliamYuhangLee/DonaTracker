730273462e48eb63d70012745c507845b4722aad
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2018-09-20 20:36:28 -0400

    Created packages

--- app/src/main/java/edu/gatech/donatracker/controller/LoginActivity.java
+++ app/src/main/java/edu/gatech/donatracker/controller/LoginActivity.java
@@ -1,4 +1,4 @@
-package edu.gatech.donatracker;
+package edu.gatech.donatracker.controller;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -31,6 +31,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.gatech.donatracker.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
