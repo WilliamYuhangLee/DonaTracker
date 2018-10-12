516f489ee378069bb4b4f2db620680daaafc8b00
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-10-12 13:52:43 -0400

    add view location list page; add view location detail page

---
+++ app/src/main/java/edu/gatech/donatracker/controller/ViewLocationDetailsActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.donatracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLocationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_location_details);
+    }
+}
---
+++ app/src/main/java/edu/gatech/donatracker/controller/ViewLocationsActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.donatracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLocationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_locations);
+    }
+}
