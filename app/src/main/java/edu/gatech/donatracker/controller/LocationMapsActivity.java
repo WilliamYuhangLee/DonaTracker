b563dd461dbad5ce11bb973167821b187b530eb8
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-11-02 20:15:52 -0400

    added map view for locations

---
+++ app/src/main/java/edu/gatech/donatracker/controller/LocationMapsActivity.java
@@ -0,0 +1,62 @@
+package edu.gatech.donatracker.controller;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.gatech.donatracker.R;
+
+public class LocationMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private double Lat;
+    private double Lng;
+    private String Name;
+    private String Phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle extra = getIntent().getExtras();
+        if (extra != null) {
+            Lat = extra.getDouble("Lat");
+            Lng = extra.getDouble("Lng");
+            Name = extra.getString("Name");
+            Phone = extra.getString("TEL");
+
+        }
+        setContentView(R.layout.activity_location_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng mLocationCord = new LatLng(Lat, Lng);
+        mMap.addMarker(new MarkerOptions().position(mLocationCord).title(Name + "\n"
+                + Phone));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mLocationCord));
+    }
+}
