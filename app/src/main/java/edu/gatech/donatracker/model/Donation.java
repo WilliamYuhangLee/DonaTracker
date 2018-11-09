cefc5115d5b8e79ecd7e4a3e4bb98793c03a0ede
QiusenHuangGT <qiusenhuang17@hotmail.com>
2018-11-09 17:20:41 -0500

    change gradle to mock log, add tests in LocationFactoryUnitTest

--- app/src/main/java/edu/gatech/donatracker/model/Donation.java
+++ app/src/main/java/edu/gatech/donatracker/model/Donation.java
@@ -16,6 +16,7 @@ import java.util.UUID;
  */
 public class Donation implements Parcelable {
 
+
     public static final Parcelable.Creator<Donation> CREATOR
             = new Parcelable.Creator<Donation>() {
         public Donation createFromParcel(Parcel in) {
--- app/src/test/java/edu/gatech/donatracker/LocationFactoryUnitTest.java
+++ app/src/test/java/edu/gatech/donatracker/LocationFactoryUnitTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.gatech.donatracker.model.Location;
+import edu.gatech.donatracker.util.LocationFactory;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,23 +16,34 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+
 public class LocationFactoryUnitTest {
 
     private static final int TIMEOUT = 200;
     private List<String[]> rawListNULL = null;
     private List<String[]> rawListNOTNULL = new ArrayList<>();
+    private String[] keys = new String[]{"Key","Name","Latitude","Longitude","Street Address"
+            ,"City","State","Zip", "Type","Phone","Website"};
+    private String[] dummyData = new String[]{"1", "AFD Station 4", "33.75416", "-84.37742"
+            , "309 EDGEWOOD AVE SE", "Atlanta", "GA", "30332", "Drop Off", "(404) 555 - 3456"
+            , "www.afd04.atl.ga"};
+
 
     @Before
     public void setUp() {
-        rawListNOTNULL.add(0
-                , new String[]{"Key","Name","Latitude","Longitude","Street Address","City","State"
-                        ,"Zip", "Type","Phone","Website"});
-
-
+        rawListNOTNULL.add(0, keys);
+        rawListNOTNULL.add(1, dummyData);
     }
 
     @Test(timeout = TIMEOUT)
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void listNotNullTest() {
+        List<Location> list = LocationFactory.parseLocations(rawListNOTNULL);
+        assertEquals(list.get(0).getKey(), Integer.parseInt(dummyData[0]));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void listNullTest() {
+        List<Location> list = LocationFactory.parseLocations(rawListNULL);
     }
 }
